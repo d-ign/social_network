37c732279d6dfc0630ff30bb347771317e28ee93
d-ign <denisignatov8@gmail.com>
2021-07-05 09:31:08 +0500

    egit forms-controls

--- src/components/common/FormsControls/FormsControls.js
+++ src/components/common/FormsControls/FormsControls.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import s from './FormsControls.module.css'
 
-export const Element = Element => ({ input, meta: { touched, error }, ...props }) => {
-  const showError = touched && error;
-  return (
-    <div className={s.formControl + " " + (showError ? s.error : "")}>
-      <Element {...input} {...props} />
-      { showError && <span>{error}</span> }
-    </div>
-  )
-};
+export const Element = Element =>
+  ({ input, meta: { touched, error }, ...props }) => {
+    const showError = touched && error;
+    // touched = значит кликнули и ушли
+    // debugger
+    return (
+      <div className={s.formControl + " " + (showError ? s.error : "")}>
+        <Element {...input} {...props} />
+        {showError && <span>{error}</span>}
+      </div>
+    )
+  };
