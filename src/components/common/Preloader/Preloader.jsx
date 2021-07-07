0ca7af7746c3fe1dcde01f3314598365e62f71c8
d-ign <denisignatov8@gmail.com>
2021-07-07 06:27:01 +0500

    styling Preloader

--- src/components/common/Preloader/Preloader.jsx
+++ src/components/common/Preloader/Preloader.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 import preloader from '../../../img/preloader.svg';
 
 const Preloader = () => {
-  return <div style={{display: 'flex', justifyContent: 'center'}}>
-    <img src={preloader} alt='preloader' />
+  return <div style={{ position: 'absolute', left: 'calc(50% - 85px)' }}>
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+      <img src={preloader} alt='preloader' />
+    </div>
   </div>
-  
+
 };
 
 export default Preloader;
