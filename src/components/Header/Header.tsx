f3271be7b8dc24cf3a9a4184f6f65b29ea3495a6
d-ign <denisignatov8@gmail.com>
2021-07-14 06:56:19 +0500

    Adding Typescript, typing Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import { NavLink, Redirect } from 'react-router-dom';
 
-
+import s from './Header.module.css';
 import logo from '../../img/icons/logo.svg';
 import logout from '../../img/icons/logout.svg';
-import s from './Header.module.css';
-
 import Button from '@material-ui/core/Button';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 
-const Header = (props) => {
+import { MapDispatchPropsType, MapStatePropsType } from './HeaderContainer';
+
+const Header: React.FC<MapStatePropsType & MapDispatchPropsType> = (props) => {
   return (
     <header className={s.header}>
       <NavLink className={s.logoAndTitle} to={'/profile'}>
