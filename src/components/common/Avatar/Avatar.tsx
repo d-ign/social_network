4755c66f6c830fd07291bdc37f0863624549c164
d-ign <denisignatov8@gmail.com>
2021-08-11 19:26:01 +0500

    fix (Avatar): show 'unknow photo' to test account

--- src/components/common/Avatar/Avatar.tsx
+++ src/components/common/Avatar/Avatar.tsx
@@ -10,20 +10,35 @@ type PropsType = {
   size: SizeType
 }
 
-const Avatar: React.FC<PropsType> = ({ photo, size, children }) => (
-  <div className={s.avatarWrap}>
-    <div
-      className={cn(
-        s.avatar,
-        { [s.large]: size === 'large' },
-        { [s.medium]: size === 'medium' },
-        { [s.small]: size === 'small' }
-      )}
-    >
-      <img src={photo || unknown} alt='avatar' />
-      {children}
+const Avatar: React.FC<PropsType> = ({ photo, size, children }) => {
+  // Проверка тестового аккаунта с id = 1079:
+  // установленное фото на тестовый аккаунт удаляется через короткое время
+  // на сервере и, если с этого аккаунта написано сообщение в чат, то
+  // при отрисовке этого сообщения (с удалённым фото) в ответ приходит
+  // ошибка вместо null | undefined, вскоре сервер опять присылает null
+  // вместо фото, но это происходит не сразу. Для этого случая эта проверка
+  let photoTrue
+  if (photo?.includes('1079')) {
+    photoTrue = null
+  } else {
+    photoTrue = photo
+  }
+
+  return (
+    <div className={s.avatarWrap}>
+      <div
+        className={cn(
+          s.avatar,
+          { [s.large]: size === 'large' },
+          { [s.medium]: size === 'medium' },
+          { [s.small]: size === 'small' }
+        )}
+      >
+        <img src={photoTrue || unknown} alt='avatar' />
+        {children}
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default Avatar
