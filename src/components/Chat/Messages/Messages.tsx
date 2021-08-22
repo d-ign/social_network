98065fd5830b58f855563c703d98c4c59e2c24a9
Denis Ignatov <denisignatov8@gmail.com>
2021-08-23 04:44:45 +0500

    refactor(NoElement, Messages): create NoElement, refactor Messages

--- src/components/Chat/Messages/Messages.tsx
+++ src/components/Chat/Messages/Messages.tsx
@@ -1,53 +1,44 @@
 import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
-import cn from 'classnames'
 import s from './Messages.module.scss'
 
 import Message from './Message/Message'
+import NoElement from '../../common/NoElement/NoElement'
 
 import { getMessages } from '../../../redux/selectors/chat-selectors'
 import { ChatMessageType } from '../../../redux/reducers/chat-reducer'
 
 const Messages: React.FC = () => {
   const messages = useSelector(getMessages)
-  const messagesAnchorRef = useRef<HTMLDivElement>(null)
+  const lastElement = useRef<HTMLDivElement>(null)
   const [isAutoScroll, setIsAutoScroll] = useState(true)
 
-  const scrollHandler = (e: React.UIEvent<HTMLDivElement, UIEvent>) => {
-    const element = e.currentTarget
-    if (
-      Math.abs(
-        element.scrollHeight - element.scrollTop - element.clientHeight
-      ) < 1
-    ) {
+  const handleScroll = (e: React.UIEvent<HTMLDivElement>) => {
+    const el = e.currentTarget
+    const scrollButton = el.scrollHeight - el.scrollTop - el.clientHeight
+
+    if (Math.abs(scrollButton) < 1) {
       if (!isAutoScroll) setIsAutoScroll(true)
     } else if (isAutoScroll) setIsAutoScroll(false)
   }
 
   useEffect(() => {
     if (isAutoScroll) {
-      messagesAnchorRef.current?.scrollIntoView({ behavior: 'smooth' })
+      lastElement.current?.scrollIntoView({ behavior: 'smooth' })
     }
   }, [messages, isAutoScroll])
 
   return (
-    <div
-      className={cn(s.container, { [s.containerNoMessages]: !messages.length })}
-      onScroll={scrollHandler}
-    >
+    <div className={s.container} onScroll={handleScroll}>
       {messages.length ? (
         messages
           .filter((m) => m.message.trim().length > 0)
           .map((m: ChatMessageType) => <Message key={m.id} message={m} />)
       ) : (
-        <div className={s.noMessages}>
-          No messages yet...
-          <br />
-          Write something!
-        </div>
+        <NoElement elements='messages' writeSomething />
       )}
-      <div ref={messagesAnchorRef} />
+      <div ref={lastElement} />
     </div>
   )
 }
---
+++ src/components/common/NoElement/NoElement.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import s from './NoElement.module.scss'
+
+type PropsType = {
+  elements: string
+  writeSomething?: boolean
+}
+
+const NoElement: React.FC<PropsType> = ({
+  elements,
+  writeSomething = false,
+}) => {
+  return (
+    <div className={s.container}>
+      <span>{`No ${elements} ${elements === 'users' ? 'found' : ''}`}</span>
+      {writeSomething && <span>Write something!</span>}
+    </div>
+  )
+}
+
+export default NoElement
