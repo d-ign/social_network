72f7de196416002abcaa9ab38af31ab8ac5a3ccf
Denis Ignatov <denisignatov8@gmail.com>
2021-10-03 09:21:53 +0500

    refactor (Chat): create separate TitleMembers comp

--- src/components/Chat/ChatMembers/ChatMembers.tsx
+++ src/components/Chat/ChatMembers/ChatMembers.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { Dispatch, SetStateAction, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 import cn from 'classnames'
@@ -13,25 +13,18 @@ import Avatar from '../../common/Avatar/Avatar'
 import Name from '../../common/Name/Name'
 
 import { getChatMembers } from '../../../redux/selectors/chat-selectors'
-import { ChatMessageAPIType, ChatMessageType } from '../../../types/types'
+import {
+  ChatMessageAPIType,
+  ChatMessageType,
+  ThemeType,
+} from '../../../types/types'
 
-const ChatMembers: React.FC<{ theme: string }> = ({ theme }) => {
+const ChatMembers: React.FC<{ theme: ThemeType }> = ({ theme }) => {
   const chatMembers = useSelector(getChatMembers)
 
   const [isShowAll, setIsShowAll] = useState(false)
   const [isReverseSort, setIsReverseSort] = useState(false)
   const [isShowMembers, setIsShowMembers] = useState(false)
-  const widthScreen = useResizeWindow()
-
-  const handleClickTitle = () => {
-    if (widthScreen <= 760) {
-      if (isShowMembers) {
-        setIsShowMembers(false)
-      } else {
-        setIsShowMembers(true)
-      }
-    }
-  }
 
   const itemsCount = isShowAll ? chatMembers.length : 2
 
@@ -42,26 +35,10 @@ const ChatMembers: React.FC<{ theme: string }> = ({ theme }) => {
 
   return (
     <section hidden={!chatMembers.length} className={s.container}>
-      <h1 className={s.visuallyHidden}>Chat members</h1>
-      {/* outer div and input for keyboard operation */}
-      <div aria-hidden='true' onChange={handleClickTitle}>
-        <input className={cn(s.visuallyHidden, s.inputHiddenBeforeFolding)} />
-        <div
-          aria-hidden='true'
-          className={cn(s.titleWrap, s.elementInteractive, {
-            [s.titleWrapDeployed]: isShowMembers,
-          })}
-          onClick={handleClickTitle}
-        >
-          <h1 className={s.title}>Members</h1>
-          <Icon
-            className={s.titleArrow}
-            path={mdiChevronDown}
-            title='ArrowShowMembers'
-            size='18px'
-          />
-        </div>
-      </div>
+      <TitleMembers
+        isShowMembers={isShowMembers}
+        setIsShowMembers={setIsShowMembers}
+      />
 
       <ChatButtonSort
         theme={theme}
@@ -91,21 +68,90 @@ const ChatMembers: React.FC<{ theme: string }> = ({ theme }) => {
               </div>
             ))}
 
-        {chatMembers.length > 2 && !isShowAll ? (
-          <div className={s.buttonShowAllWrap}>
-            <button
-              type='button'
-              className={s.buttonShowAll}
-              onClick={() => setIsShowAll(true)}
-            >
-              show all {chatMembers.length} ...
-            </button>
-          </div>
-        ) : (
-          ''
-        )}
+        <ButtonShowAll
+          chatMembers={chatMembers}
+          isShowAll={isShowAll}
+          setIsShowAll={setIsShowAll}
+        />
       </div>
     </section>
+  )
+}
+
+type TitleMembersPropsType = {
+  isShowMembers: boolean
+  setIsShowMembers: Dispatch<SetStateAction<boolean>>
+}
+
+const TitleMembers: React.FC<TitleMembersPropsType> = ({
+  isShowMembers,
+  setIsShowMembers,
+}) => {
+  const widthScreen = useResizeWindow()
+
+  const handleClickTitle = () => {
+    if (widthScreen <= 760) {
+      if (isShowMembers) {
+        setIsShowMembers(false)
+      } else {
+        setIsShowMembers(true)
+      }
+    }
+  }
+
+  return (
+    <>
+      <h1 className={s.visuallyHidden}>Chat members</h1>
+      {/* outer div and input for keyboard operation */}
+      <div aria-hidden='true' onChange={handleClickTitle}>
+        <input className={cn(s.visuallyHidden, s.inputHiddenBeforeFolding)} />
+        <div
+          aria-hidden='true'
+          className={cn(s.titleWrap, s.elementInteractive, {
+            [s.titleWrapDeployed]: isShowMembers,
+          })}
+          onClick={handleClickTitle}
+        >
+          <h1 className={s.title}>Members</h1>
+          <Icon
+            className={s.titleArrow}
+            path={mdiChevronDown}
+            title='ArrowShowMembers'
+            size='18px'
+          />
+        </div>
+      </div>
+    </>
+  )
+}
+
+type ButtonShowAllPropsType = {
+  chatMembers: ChatMessageType[]
+  isShowAll: boolean
+  setIsShowAll: Dispatch<SetStateAction<boolean>>
+}
+
+const ButtonShowAll: React.FC<ButtonShowAllPropsType> = ({
+  chatMembers,
+  isShowAll,
+  setIsShowAll,
+}) => {
+  return (
+    <>
+      {chatMembers.length > 2 && !isShowAll ? (
+        <div className={s.buttonShowAllWrap}>
+          <button
+            type='button'
+            className={s.buttonShowAll}
+            onClick={() => setIsShowAll(true)}
+          >
+            show all {chatMembers.length} ...
+          </button>
+        </div>
+      ) : (
+        ''
+      )}
+    </>
   )
 }
 
